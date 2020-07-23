8aee3b3e834d7978c7ae2e70b82d0660392421f9
Zeeshan Ali <zeeshan2193@gmail.com>
2020-07-23 20:35:53 +0500

    Removing All Warnings
    All warnings are removed

--- src/components/Background.js
+++ src/components/Background.js
@@ -3,11 +3,7 @@ import React from 'react'
 export const Background = () => {
     return (
         <div>
-           <div className="scenery background1">
-        <img className="pawn" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png" alt=" " />
-        <img className="pawn2" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png" alt=" " />
-        <img className="treeback" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png" alt=" " />
-      </div>
+          
         </div>
     )
 }
