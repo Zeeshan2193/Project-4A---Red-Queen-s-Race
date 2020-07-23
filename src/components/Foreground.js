d8a9e54394c2c18c2886b7c039eea7514b26321f
Zeeshan Ali <zeeshan2193@gmail.com>
2020-07-23 20:24:49 +0500

    Project 4A - Red Queen’s Race
    Project 4A - Red Queen’s Race is created with the help of Web Animation Api

---
+++ src/components/Foreground.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Foreground = () => {
+    return (
+        <div>
+             <div className="scenery" id="foreground">
+        <img id="treefore" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png" alt=" "/>
+      </div>
+            
+        </div>
+    )
+}
---
+++ src/components/Sky.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Sky = () => {
+    return (
+        <div>
+            <div className="sky">
+                <img src = "https://data.whicdn.com/images/157190931/original.gif" alt="sky" />
+            </div>
+        </div>
+    )
+}
